b87347ebbd551c1fde224a05dbfc4de4ef0f2bbd
rochak-chadha <rchadha@4moms.com>
2015-01-18 20:04:50 -0500

    Update OrigamiController.py

--- origami_wearable_demo/OrigamiController.py
+++ origami_wearable_demo/OrigamiController.py
@@ -1,3 +1,22 @@
+from time import sleep
+import os
+import RPi.GPIO as GPIO
+
+REV1 = 3
+REV2 = 2
+BUTTON = 17
+LIGHTS = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(REV1,GPIO.OUT) #Rev 2 
+GPIO.setup(REV2,GPIO.OUT) #Rev 1
+GPIO.setup(BUTTON,GPIO.OUT) #Dial Switch
+GPIO.setup(LIGHTS,GPIO.OUT) # Lights
+
+GPIO.output(REV1, True)
+GPIO.output(REV2, True)
+GPIO.output(BUTTON, False)
+GPIO.output(LIGHTS, False)
 
 class OrigamiController():
 
@@ -7,10 +26,33 @@ class OrigamiController():
 
     def actuate(self):
         print("Actuating Origami")
+        
+        GPIO.Output(REV1, False)
+        time.sleep(1)
+        GPIO.Output(REV1, True)
+        time.sleep(1)
+        
+        GPIO.Output(REV2, False)
+        time.sleep(1)
+        GPIO.Output(REV2, True)
+        time.sleep(1)
+        
+        GPIO.Output(BUTTON, True)
+        time.sleep(1)
+        GPIO.Output(BUTTON, False)
+        time.sleep(1)
+        
+        
+        
 
     def turn_lights_on(self):
         print("Turning light on")
+        
+        GPIO.Output(LIGHTS, True)
+        
 
     def turn_lights_off(self):
         print("Turning lights off")
+        
+        GPIO.Output(LIGHTS, False)
 
